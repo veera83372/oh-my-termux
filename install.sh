e9f5d03332ff33e8243ee1f8fe43e0193c16740b
veera venky <veeravenky222@gmail.com>
2017-10-04 00:59:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 curl -fsLo $HOME/.termux/colors.properties --create-dirs https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/colors.properties
 curl -fsLo $HOME/.termux/font.ttf --create-dirs https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/font.ttf
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
+git clone -b master  git://github.com/veera83372/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 chsh -s zsh
 
